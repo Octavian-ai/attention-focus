eeb7a458a7c1e148c70d364e9e685229ec70f5b7
eastlondoner <andy@octavian>
2018-09-24 22:05:31 +0100

    latest

--- build-data.sh
+++ build-data.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 pipenv run python -m src.build_data.build
+
+
+Orthogonal
+non orthogonal
+random
+
+vary length of vector 3 - 100?
+
+change the outpuc cell
+change activation function
+turn relu on/off
+
+accuracy after time
