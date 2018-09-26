968882aa11121b25136181173b85eab8ba85f5f4
eastlondoner <andy@octavian>
2018-09-26 19:39:45 +0100

    some experiment scripts

---
+++ 00-no-focus-tanh-orthogonal.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Add results below and git commit when you have
+# accuracy, Model directory
+# e.g. 98% eeb7a45/model/25633113
+
+pipenv run python -m src.experiments.run \
+  --use-attention-focus=False \
+  --max-steps=500000 \
+  --kb-vector-type='orthogonal' \
+  --number-of-questions=5000 \
+  --kb-vector-length=12 \
+  --kb-list-size=2 \
+  --attention-output-activation=tanh
